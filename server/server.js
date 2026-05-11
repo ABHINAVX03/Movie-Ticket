82b629cfe5dcaaddd06848f03b8018b63273acc1
Abhinav Gupta <98380157+ABHINAVX03@users.noreply.github.com>
2026-05-12 01:26:16 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import { functions, inngest } from './inngest/index.js';
-import {serve} from 'inngest/express'
+import { serve } from 'inngest/express'
 import publicRoutes from './routes/publicRoutes.js';
 import adminRoutes from './routes/adminRoutes.js';
 dotenv.config();
@@ -19,13 +19,18 @@ const startServer = async () => {
     await connectDB();
     //MiddleWare
     app.use(express.json());
-    app.use(cors());
+    app.use(cors({
+      origin: [
+        'http://localhost:5173',
+        'https://movie-ticket-brown.vercel.app'
+      ],
+    }));
     app.use(clerkMiddleware())
     // API routing
     app.get('/', (req, res) => res.send('Server is Live!'));
     app.use('/api', publicRoutes);
     app.use('/api/admin', adminRoutes);
-    app.use('/api/inngest',serve({client:inngest,functions}))
+    app.use('/api/inngest', serve({ client: inngest, functions }))
     app.listen(port, () => {
       console.log(`🚀 Server listening at http://localhost:${port}`);
     });
